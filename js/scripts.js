da2a8be523f3f15cc367bd34bfa3538eba61cb95
Max Betich <betichm@gmail.com>
2023-04-07 11:34:09 -0700

    add orderSubmit function

--- js/scripts.js
+++ js/scripts.js
@@ -56,8 +56,38 @@ function pizzaCreator() {
 
 function pizzaCost() {
   let newPizza = pizzaCreator();
-  newPizza.sizeCost();
-  newPizza.toppingsCost();
-  newPizza.overallCost();
-  return newPizza.totalCost;
+  if (newPizza !== undefined) {
+    newPizza.sizeCost();
+    newPizza.toppingsCost();
+    newPizza.overallCost();
+    return newPizza.totalCost;
+  }
 }
+
+function orderSubmit(event) {
+  event.preventDefault();
+  const orderPizza = pizzaCreator();
+  const orderCost = pizzaCost();
+  const outputDiv = document.getElementById("order-output");
+  outputDiv.innerText = "";
+  const newH4 = document.createElement("h4");
+  newH4.innerText = "Your Order:"
+  outputDiv.append(newH4);
+  const newP = document.createElement("p");
+  newP.innerText = "Size: " + (orderPizza.size).toUpperCase();
+  outputDiv.append(newP);
+  const newP2 = document.createElement("p");
+  newP2.innerText = "Toppings:"
+  outputDiv.append(newP2);
+  const newUl = document.createElement("ul");
+  orderPizza.toppings.forEach(topping => {
+    let newLi = document.createElement("li");
+    newLi.append(topping);
+    newUl.append(newLi);
+  });
+  outputDiv.append(newUl);
+  const newP3 = document.createElement("p");
+  newP3.innerText = "Cost: $" + orderCost.toFixed(2);
+  outputDiv.append(newP3);
+}
+
